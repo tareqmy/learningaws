9a659aab85ea991a5766dd0be23343d505b18837
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2021-06-02 19:32:27 +0600

    the initial commit with spring web and thymeleaf

---
+++ src/main/java/com/example/learningaws/LearningawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningaws/LearningawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
